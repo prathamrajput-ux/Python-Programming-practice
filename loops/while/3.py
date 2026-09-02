517c44354358932d23b040fa9439f86b5afc0a87
prathamrajput-ux <prathamsinghujjain@gmail.com>
2026-09-02 08:53:42 +0530

    questions of while loop

---
+++ loops/while/3.py
@@ -0,0 +1,11 @@
+#sum and average of fisrt 10 element
+
+i = 0
+s = 0
+
+while(i<=10):
+    s =s +i
+    i = i + 1 
+avg = float(s)/10
+print("Sum of all number is : ",s)
+print("Average of all number is : ",avg)
---
+++ loops/while/4.py
@@ -0,0 +1,5 @@
+# 20 horizontral asterrisks
+i = 0
+while(i<=20):
+    print("*",end = "")
+    i = i+1
